df9d71bdf43909d44e2923487891c8afcf9a84e6
dragang1 <gajicdragan22@gmail.com>
2024-02-29 23:38:38 +0100

    footer.

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -22,17 +22,17 @@ function FooterComponent() {
                 {/* content fotter */}
                 <div className='mt-[50px] flex flex-col md:flex-row  '>
                     {/* left side */}
-                    <div className='flex flex-row md:flex-col gap-[20px] w-full md:w-[20%] mb-[40px] px-[20px]'>
-                        <img src={logo} alt='myLogo' className='w-[150px]' />
+                    <div className='flex flex-row items-center md:flex-col gap-[20px] w-full md:w-[20%] mb-[40px] px-[20px]'>
+                        <img src={logo} alt='myLogo' className='w-[150px] ' />
 
-                        <div className='flex items-center gap-[15px]'>
+                        <div className='flex justify-center items-center gap-[15px]'>
                             <FaFacebook size={24} color='#EDA415' />
                             <FaInstagram size={24} color='#EDA415' />
                         </div>
                     </div>
 
                     {/* right side */}
-                    <div className='flex items-start gap-[30px] w-full md:w-[80%] justify-between px-[20px] text-[12px]'>
+                    <div className='flex items-start gap-[30px] w-full md:w-[80%] justify-between px-[20px] '>
                         <FooterListComponent
                             title='Find Products'
                             items={footerContent}
